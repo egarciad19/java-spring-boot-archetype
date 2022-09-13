f4f84c74f90fe097f4c8753788ffa61aebcb51ec
egarciad19 <egarciad19@miumg.edu.gt>
2022-09-12 18:26:53 -0600

    Se agrega Anotacion de CrossOrgin en controlador

--- src/main/java/com/dase/gestortareas/controllers/UserController.java
+++ src/main/java/com/dase/gestortareas/controllers/UserController.java
@@ -6,6 +6,7 @@ import io.swagger.annotations.ApiParam;
 import java.util.Locale;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 
-//@CrossOrigin(origins="http://localhost:4200")
+@CrossOrigin(origins="https://analisis-sistemas.herokuapp.com")
 @RestController
 
 @RequestMapping(path = "/user")
